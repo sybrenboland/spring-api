54aa1b247142eed5822c61265ef59e2aefc093ba
sybrenboland <sybboland@gmail.com>
2019-02-01 09:23:46 +0100

    Added kubernetes deployment scripts

--- src/main/java/org/shboland/PersonController.java
+++ src/main/java/org/shboland/PersonController.java
@@ -1,5 +1,6 @@
 package org.shboland;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
+    @Value("spring.application.name")
+    private String applicationName;
+
     @RequestMapping("")
     public String greeting(@RequestParam(value="name", defaultValue = "you") String name) {
-        return "Hello " + name;
+        return "Hello " + name + "! Here's " + applicationName;
     }
 }
